4ef55daccdc24cafcd0afd3204fe1087bc574916
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-12-07 10:17:02 +0100

    update install.sh

--- bin.sh
+++ bin.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
-if ! command -v apache2 &> /dev/null
-then
-    echo "apache2 n'est pas installé. Installation en cours..."
-    sudo apt-get update
-    sudo apt-get install -y apache2
+if [ ! -d "mgrosmann" ]; then
+  git clone https://github.com/mgrosmann/docker.git
 else
-    echo "apache2 est déjà installé."
+  echo "Le répertoire mgrosmann existe déjà."
 fi
 if ! command -v dos2unix &> /dev/null
 then
-    echo "dos2unix n'est pas installé. Installation en cours..."
     sudo apt-get install -y dos2unix
 else
     echo "dos2unix est déjà installé."
 fi
-if ! ls -r | grep -q "mgrosmann"; then
-  git clone https://github.com/mgrosmann/mgrosmann.git
+if ! command -v docker &> /dev/null
+then
+    chmod +x install.sh
+    bash install.sh
+else
+    echo "docker est déjà installé."
 fi
-cd mgrosmann/docker
-chmod +x /*.sh
+cd docker
+chmod +x *.sh
 dos2unix *.sh
 for script in *.sh
 do
-  mv "$file" "/usr/local/bin/$(basename "$file" .sh)"
+  mv "$script" "/usr/local/bin/$(basename "$script" .sh)"
 done
--- install.sh
+++ install.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
-
-# Mettre à jour le système
+if [ -f /etc/debian_version ]; then
+  OS="Debian"
+elif [ -f /etc/lsb-release ]; then
+  . /etc/lsb-release
+  if [ "$DISTRIB_ID" == "Ubuntu" ]; then
+    OS="Ubuntu"
+  fi
+else
+  echo "Ce script ne supporte que Debian et Ubuntu."
+  exit 1
+fi
 sudo apt-get update && sudo apt-get upgrade -y
-
-# Installer les dépendances nécessaires
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-
-# Ajouter la clé GPG officielle de Docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-# Ajouter le dépôt Docker
-sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-# Installer Docker
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common gnupg
+if [ "$OS" == "Debian" ]; then
+  apt install sudo
+  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+elif [ "$OS" == "Ubuntu" ]; then
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+fi
 sudo apt-get update
-sudo apt-get install -y docker-ce
-
-# Démarrer et activer Docker
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo systemctl start docker
 sudo systemctl enable docker
-
-# Ajouter l'utilisateur actuel au groupe Docker
 sudo usermod -aG docker ${USER}
-
 echo "Docker a été installé et configuré avec succès. Veuillez vous déconnecter et vous reconnecter pour appliquer les changements de groupe."
