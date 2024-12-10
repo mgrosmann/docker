f536f8d80cd12973e00e73e271b167574f04bef4
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-12-10 14:02:07 +0100

    Update nextcloud.sh

--- nextcloud.sh
+++ nextcloud.sh
@@ -53,5 +53,4 @@ CREATE DATABASE nextcloud;
 CREATE USER 'nextcloud'@'localhost' IDENTIFIED BY 'nextcloud';
 GRANT ALL PRIVILEGES ON nextcloud.* TO 'nextcloud'@'localhost';
 FLUSH PRIVILEGES;
-EXIT;
 EOF
