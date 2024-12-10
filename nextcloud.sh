f02e4a6ee4856f269b490862fc03914ab6b1f72c
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-12-10 20:56:05 +0100

    Update nextcloud.sh

--- nextcloud.sh
+++ nextcloud.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-
 read -p "Port MySQL: " port_sql
 read -p "Port phpMyAdmin: " port_pma
 read -p "Port Nextcloud: " port_nextcloud
 read -p "Nom du projet: " name
 read -p "Mot de passe du compte root: " root
 
-cat <<EOF > docker-$name.yaml
+cat > docker-$name.yaml <<EOL
 services:
   mysql_$name:
     image: mysql
     container_name: db_$name
     environment:
       MYSQL_ROOT_PASSWORD: $root
+      MYSQL_DATABASE: nextcloud
+      MYSQL_USER: nextcloud
+      MYSQL_PASSWORD: nextcloud
     ports:
       - "$port_sql:3306"
     networks:
@@ -46,5 +48,14 @@ services:
 networks:
   network_$name:
     driver: bridge
-EOF
+EOL
+
 docker compose -f "docker-$name.yaml" up -d
+
+sleep 20 
+
+docker exec -i db_$name mysql -u root -p$root <<EOF
+GRANT ALL PRIVILEGES ON nextcloud.* TO 'nextcloud'@'%';
+FLUSH PRIVILEGES;
+EXIT;
+EOF
