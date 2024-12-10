cdc09ffeb4cca7a6cdc4c0abd21899aba10ba0d2
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-12-10 12:35:53 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,29 +1,13 @@
 #!/bin/bash
-if [ -f /etc/debian_version ]; then
-  OS="Debian"
-elif [ -f /etc/lsb-release ]; then
-  . /etc/lsb-release
-  if [ "$DISTRIB_ID" == "Ubuntu" ]; then
-    OS="Ubuntu"
-  fi
+read -p "Veuillez entrer votre distribution Linux (1 pour Ubuntu et 2 pour Debian) : " distribution
+
+# Appliquer chmod en fonction de la distribution
+if [ "$distribution" = "1" ]; then
+    chmod +x docker.sh
+    bash docker.sh
+elif [ "$distribution" = "2" ]; then
+    chmod +x deb_docker.sh
+    bash deb_docker.sh
 else
-  echo "Ce script ne supporte que Debian et Ubuntu."
-  exit 1
+    echo "Choix non pris en compte"
 fi
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common gnupg
-if [ "$OS" == "Debian" ]; then
-  apt install sudo
-  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-  echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-elif [ "$OS" == "Ubuntu" ]; then
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-  sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-fi
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-sudo systemctl start docker
-sudo systemctl enable docker
-usermod -aG docker www-data
-sudo usermod -aG docker ${USER}
-echo "Docker a été installé et configuré avec succès. Veuillez vous déconnecter et vous reconnecter pour appliquer les changements de groupe."
