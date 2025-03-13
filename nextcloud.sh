d6b90ebcf7466f3ce4e8974e6a8126530fa3a817
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-13 22:00:46 +0100

    Update nextcloud.sh

--- nextcloud.sh
+++ nextcloud.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-read -p "Port Nextcloud: " port_nextcloud
-read -p "Nom du projet: " name
-read -p "Mot de passe du compte root: " root
-read -p "port sql: " port_sql
-read -p "port pma: " port_pma
-
+port_nextcloud=$(dialog --inputbox "Entrez le port Nextcloud :" 8 50 2>&1 >/dev/tty)
+name=$(dialog --inputbox "Entrez le nom du projet :" 8 50 2>&1 >/dev/tty)
+root=$(dialog --inputbox "Entrez le mot de passe du compte root MySQL :" 8 50 2>&1 >/dev/tty)
+port_sql=$(dialog --inputbox "Entrez le port SQL :" 8 50 2>&1 >/dev/tty)
+port_pma=$(dialog --inputbox "Entrez le port phpMyAdmin :" 8 50 2>&1 >/dev/tty)
 cat > docker-$name.yaml <<EOL
 services:
 
@@ -42,4 +41,10 @@ networks:
   network_$name:
     driver: bridge
 EOL
+
+dialog --msgbox "Le fichier docker-$name.yaml a été créé avec succès !" 8 50
+
 docker compose -f "docker-$name.yaml" up -d
+
+dialog --msgbox "Les conteneurs Nextcloud, MySQL et phpMyAdmin pour le projet $name ont été démarrés avec succès !" 8 50
+clear
