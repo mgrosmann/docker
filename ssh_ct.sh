8e853f0cbed2ee7ce08310990f7cc7f3f808d00a
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-13 21:58:43 +0100

    Update ssh_ct.sh

--- ssh_ct.sh
+++ ssh_ct.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-read -p "À quel conteneur voulez-vous accéder ? " container_name
-docker container exec -it $container_name /bin/bash
-
-
-
+container_name=$(dialog --inputbox "Entrez le nom du conteneur auquel vous souhaitez accéder :" 8 50 2>&1 >/dev/tty)
+if [ -n "$container_name" ]; then
+    docker container exec -it "$container_name" /bin/bash
+else
+    dialog --msgbox "Nom de conteneur invalide ou vide. Veuillez réessayer." 8 50
+fi
+clear
