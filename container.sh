87865b48e0f9e82f4503888f58e0f22799f3dcd2
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-13 18:36:06 +0100

    Update container.sh

--- container.sh
+++ container.sh
@@ -1,121 +1,91 @@
 #!/bin/bash
-
-# Couleurs pour la mise en forme
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-BLUE='\033[0;34m'
-PINK='\033[1;35m'
-LIGHT_GREEN='\033[1;32m'
-LIGHT_BLUE='\033[1;34m'
-GRAY='\033[1;30m'
-ORANGE='\033[0;33m'
-NC='\033[0m'
-
-# Menu principal : Choix de la catégorie
-echo -e "${LIGHT_GREEN}Veuillez choisir une catégorie :${NC}"
-echo -e "${RED}1)${NC} Création/installation de conteneurs"
-echo -e "${RED}2)${NC} Gestion des conteneurs existants"
-echo -e "${RED}3)${NC} Outils de gestion Docker"
-read -p "Entrez le numéro de votre choix : " categorie
-
-# Selon le choix de catégorie
-if [ "$categorie" -eq 1 ]; then
-    echo -e "\n${LIGHT_GREEN}Création/installation de conteneurs :${NC}"
-    echo -e "  ${RED}1)${NC} Créer un nouveau ${GREEN}conteneur${NC} avec fichier yaml"
-    echo -e "  ${RED}2)${NC} Installer le ${GREEN}conteneur multi-service${NC} (${PINK}MySQL${NC}, ${GRAY}phpMyAdmin${NC}, ${ORANGE}FTP${NC}, ${LIGHT_BLUE}APACHE${NC})"
-    echo -e "  ${RED}3)${NC} Installer un ${GREEN}conteneur${NC}  en ${RED}session interactive${NC} ou ${ORANGE}détaché${NC}"
-    echo -e "  ${RED}4)${NC} Supprimer un ${GREEN}conteneur${NC}"
-    echo -e "  ${RED}5)${NC} Supprimer une ${GREEN}image Docker${NC}"
-    read -p "Entrez le numéro de votre choix : " choix
-    if [ "$choix" -eq 1 ]; then
-        compose_aio
-    elif [ "$choix" -eq 2 ]; then
-        compose
-    elif [ "$choix" -eq 3 ]; then
-        docker_aio
-    elif [ "$choix" -eq 4 ]; then
-        read -p "Entrez l'ID ou le nom du conteneur à supprimer : " container_id
-        confirm
-        docker rm -f $container_id
-    elif [ "$choix" -eq 5 ]; then
-        read -p "Entrez l'ID ou le nom de l'image Docker à supprimer : " image_id
-        confirm
-        docker rmi $image_id
-    else
-        echo -e "${RED}Choix invalide dans cette catégorie.${NC}"
-    fi
-
-elif [ "$categorie" -eq 2 ]; then
-    echo -e "\n${LIGHT_GREEN}Gestion des conteneurs existants :${NC}"
-    echo -e "  ${RED}1)${NC} Se connecter à un ${GREEN}conteneur${NC}"
-    echo -e "  ${RED}2)${NC} Démarrer tous les ${GREEN}conteneurs${NC}"
-    echo -e "  ${RED}3)${NC} Arrêter tous les ${GREEN}conteneurs${NC}"
-    echo -e "  ${RED}4)${NC} Installer ${BLUE}nano${NC} et ${YELLOW}wget${NC} sur un ${GREEN}conteneur${NC}"
-    echo -e "  ${RED}5)${NC} Vérifier et recréer les ${GREEN}réseaux Docker manquants${NC} pour les conteneurs arrêtés"
-    echo -e "  ${RED}6)${NC} Supprimer tous les ${GREEN}conteneurs arrêtés${NC}"
-    echo -e "  ${RED}7)${NC} Recréer les ${GREEN}conteneurs arrêtés${NC} basés sur le fichier de configuration"
-    read -p "Entrez le numéro de votre choix : " choix
-    if [ "$choix" -eq 1 ]; then
-        ssh_ct
-    elif [ "$choix" -eq 2 ]; then
-        start
-    elif [ "$choix" -eq 3 ]; then
-        stop
-        docker ps -q | xargs -r docker stop
-    elif [ "$choix" -eq 4 ]; then
-        linux
-    elif [ "$choix" -eq 5 ]; then
-        network
-    elif [ "$choix" -eq 6 ]; then
-        remove
-    elif [ "$choix" -eq 7 ]; then
-        docker-compose up -d
-    else
-        echo -e "${RED}Choix invalide dans cette catégorie.${NC}"
-    fi
-
-elif [ "$categorie" -eq 3 ]; then
-    echo -e "\n${LIGHT_GREEN}Outils de gestion Docker :${NC}"
-    echo -e "  ${RED}1)${NC} Afficher les ${GREEN}logs${NC} d'un conteneur"
-    echo -e "  ${RED}2)${NC} Lister tous les ${GREEN}volumes${NC}"
-    echo -e "  ${RED}3)${NC} Lister toutes les ${GREEN}images${NC}"
-    echo -e "  ${RED}4)${NC} Lister tous les ${GREEN}conteneurs${NC}"
-    echo -e "  ${RED}5)${NC} Afficher les ${GREEN}statistiques Docker${NC}"
-    echo -e "  ${RED}6)${NC} Inspecter un ${GREEN}conteneur${NC}"
-    echo -e "  ${RED}7)${NC} Sauvegarder et restaurer un ${GREEN}conteneur${NC}"
-    read -p "Entrez le numéro de votre choix : " choix
-    if [ "$choix" -eq 1 ]; then
-        read -p "Entrez l'ID ou le nom du conteneur : " container_id
-        docker logs $container_id
-    elif [ "$choix" -eq 2 ]; then
-        docker volume ls
-    elif [ "$choix" -eq 3 ]; then
-        docker images
-    elif [ "$choix" -eq 4 ]; then
-        docker ps -a
-    elif [ "$choix" -eq 5 ]; then
-        docker stats
-    elif [ "$choix" -eq 6 ]; then
-        read -p "Entrez l'ID ou le nom du conteneur : " container_id
-        docker inspect $container_id
-    elif [ "$choix" -eq 7 ]; then
-        echo -e "${LIGHT_GREEN}1) Sauvegarder un conteneur"
-        echo -e "2) Restaurer un conteneur${NC}"
-        read -p "Entrez le numéro de votre choix : " sub_choix
-        if [ "$sub_choix" -eq 1 ]; then
-            read -p "Entrez l'ID ou le nom du conteneur : " container_id
-            docker commit $container_id ${container_id}_backup
-            docker save -o ${container_id}_backup.tar ${container_id}_backup
-        elif [ "$sub_choix" -eq 2 ]; then
-            read -p "Entrez le chemin du fichier de sauvegarde : " backup_path
-            docker load -i $backup_path
-        else
-            echo -e "${RED}Choix invalide.${NC}"
-        fi
-    else
-        echo -e "${RED}Choix invalide dans cette catégorie.${NC}"
-    fi
-else
-    echo -e "${RED}Choix de catégorie invalide. Veuillez réessayer.${NC}"
-fi
+DIALOG=${DIALOG=dialog}
+categorie=$($DIALOG --clear --backtitle "Gestion Docker" \
+    --title "Gestion centralisé Docker" \
+    --menu "Veuillez choisir une catégorie :" 15 50 3 \
+    1 "Création/installation de conteneurs" \
+    2 "Gestion des conteneurs existants" \
+    3 "Outils de gestion Docker" \
+    2>&1 >/dev/tty)
+clear
+case $categorie in
+    1)
+        choix=$($DIALOG --clear --backtitle "Création/installation de conteneurs" \
+            --title "Choisissez une action" \
+            --menu "Veuillez choisir une option :" 15 50 5 \
+            1 "Créer un conteneur avec fichier yaml" \
+            2 "Installer un conteneur multi-service (MySQL, phpMyAdmin, FTP, Apache)" \
+            3 "Installer un conteneur en session interactive ou détaché" \
+            4 "Supprimer un conteneur" \
+            5 "Supprimer une image Docker" \
+            2>&1 >/dev/tty)
+        clear
+        case $choix in
+            1) compose_aio ;;
+            2) compose ;;
+            3) docker_aio ;;
+            4) container_id=$($DIALOG --inputbox "Entrez l'ID ou le nom du conteneur à supprimer :" 8 40 2>&1 >/dev/tty)
+               confirm && docker rm -f $container_id ;;
+            5) image_id=$($DIALOG --inputbox "Entrez l'ID ou le nom de l'image Docker à supprimer :" 8 40 2>&1 >/dev/tty)
+               confirm && docker rmi $image_id ;;
+        esac ;;
+    2)
+        choix=$($DIALOG --clear --backtitle "Gestion des conteneurs existants" \
+            --title "Choisissez une action" \
+            --menu "Veuillez choisir une option :" 15 50 7 \
+            1 "Se connecter à un conteneur" \
+            2 "Démarrer tous les conteneurs" \
+            3 "Arrêter tous les conteneurs" \
+            4 "Installer nano et wget sur un conteneur" \
+            5 "Vérifier et recréer les réseaux Docker manquants" \
+            6 "Supprimer tous les conteneurs arrêtés" \
+            7 "Recréer les conteneurs arrêtés" \
+            2>&1 >/dev/tty)
+        clear
+        case $choix in
+            1) ssh_ct ;;
+            2) start ;;
+            3) stop && docker ps -q | xargs -r docker stop ;;
+            4) linux ;;
+            5) network ;;
+            6) remove ;;
+            7) docker-compose up -d ;;
+        esac ;;
+    3)
+        choix=$($DIALOG --clear --backtitle "Outils de gestion Docker" \
+            --title "Choisissez une action" \
+            --menu "Veuillez choisir une option :" 15 50 7 \
+            1 "Afficher les logs d'un conteneur" \
+            2 "Lister tous les volumes" \
+            3 "Lister toutes les images" \
+            4 "Lister tous les conteneurs" \
+            5 "Afficher les statistiques Docker" \
+            6 "Inspecter un conteneur" \
+            7 "Sauvegarder et restaurer un conteneur" \
+            2>&1 >/dev/tty)
+        clear
+        case $choix in
+            1) container_id=$($DIALOG --inputbox "Entrez l'ID ou le nom du conteneur :" 8 40 2>&1 >/dev/tty)
+               docker logs $container_id ;;
+            2) docker volume ls ;;
+            3) docker images ;;
+            4) docker ps -a ;;
+            5) docker stats ;;
+            6) container_id=$($DIALOG --inputbox "Entrez l'ID ou le nom du conteneur :" 8 40 2>&1 >/dev/tty)
+               docker inspect $container_id ;;
+            7) sub_choix=$($DIALOG --clear --backtitle "Sauvegarde et restauration" \
+                --title "Choisissez une option" \
+                --menu "Veuillez choisir une option :" 10 40 2 \
+                1 "Sauvegarder un conteneur" \
+                2 "Restaurer un conteneur" \
+                2>&1 >/dev/tty)
+               clear
+               case $sub_choix in
+                   1) container_id=$($DIALOG --inputbox "Entrez l'ID ou le nom du conteneur :" 8 40 2>&1 >/dev/tty)
+                      docker commit $container_id ${container_id}_backup
+                      docker save -o ${container_id}_backup.tar ${container_id}_backup ;;
+                   2) backup_path=$($DIALOG --inputbox "Entrez le chemin du fichier de sauvegarde :" 8 40 2>&1 >/dev/tty)
+                      docker load -i $backup_path ;;
+               esac ;;
+        esac ;;
+esac
