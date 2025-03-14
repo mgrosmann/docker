f1d9dd900941c3f6adaae59a01833afb31e85e89
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-14 10:01:35 +0100

    Update gestion.sh

--- gestion.sh
+++ gestion.sh
@@ -1,1 +1,26 @@
+#!/bin/bash
+DIALOG=${DIALOG=dialog}
+
+choix=$($DIALOG --clear --backtitle "Gestion des conteneurs existants" \
+    --title "Choisissez une action" \
+    --menu "Veuillez choisir une option :" 15 50 7 \
+    1 "Se connecter à un conteneur" \
+    2 "Démarrer tous les conteneurs" \
+    3 "Arrêter tous les conteneurs" \
+    4 "Installer nano et wget sur un conteneur" \
+    5 "Vérifier et recréer les réseaux Docker manquants" \
+    6 "Supprimer tous les conteneurs arrêtés" \
+    7 "Recréer les conteneurs arrêtés" \
+    2>&1 >/dev/tty)
+clear
+
+case $choix in
+    1) ssh_ct ;;
+    2) start ;;
+    3) stop && docker ps -q | xargs -r docker stop ;;
+    4) linux ;;
+    5) network ;;
+    6) remove ;;
+    7) docker-compose up -d ;;
+esac
 
