466ed179c81131f0d77ae93940b3c56a6cfe3a40
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-14 10:27:42 +0100

    test

--- linux.sh
+++ linux.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-read -p "Sur quel conteneur voulez-vous appliquer des commandes Linux : " name
-docker exec -it "$name" /bin/bash -c "apt-get update && apt-get install -y wget nano"
-echo "Commandes Linux installées avec succès dans le conteneur $name"
+container_name=$(dialog --inputbox "Sur quel conteneur voulez-vous appliquer des commandes Linux :" 8 50 2>&1 >/dev/tty)
+if [ $? -ne 0 ]; then
+    gestion
+    exit
+fi
+if [ -n "$container_name" ]; then
+    docker exec -it "$name" /bin/bash -c "apt-get update && apt-get install -y wget nano"
+else
+    dialog --msgbox "Nom de conteneur invalide ou vide. Veuillez réessayer." 8 50
+fi
+clear
