36f3e3d3203b7ed29d60b65608355209f929e065
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-14 10:01:00 +0100

    Update utils.sh

--- utils.sh
+++ utils.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
 DIALOG=${DIALOG=dialog}
-choix=$($DIALOG --clear --backtitle "Gestion des conteneurs existants" \
+
+choix=$($DIALOG --clear --backtitle "Outils de gestion Docker" \
     --title "Choisissez une action" \
     --menu "Veuillez choisir une option :" 15 50 7 \
-    1 "Se connecter à un conteneur" \
-    2 "Démarrer tous les conteneurs" \
-    3 "Arrêter tous les conteneurs" \
-    4 "Installer nano et wget sur un conteneur" \
-    5 "Vérifier et recréer les réseaux Docker manquants" \
-    6 "Supprimer tous les conteneurs arrêtés" \
-    7 "Recréer les conteneurs arrêtés" \
+    1 "Afficher les logs d'un conteneur" \
+    2 "Lister tous les volumes" \
+    3 "Lister toutes les images" \
+    4 "Lister tous les conteneurs" \
+    5 "Afficher les statistiques Docker" \
+    6 "Inspecter un conteneur" \
+    7 "Sauvegarder et restaurer un conteneur" \
     2>&1 >/dev/tty)
 clear
 
 case $choix in
-    1) ssh_ct ;;
-    2) start ;;
-    3) stop && docker ps -q | xargs -r docker stop ;;
-    4) linux ;;
-    5) network ;;
-    6) remove ;;
-    7) docker-compose up -d ;;
+    1) container_id=$($DIALOG --inputbox "Entrez l'ID ou le nom du conteneur :" 8 40 2>&1 >/dev/tty)
+       docker logs $container_id ;;
+    2) docker volume ls ;;
+    3) docker images ;;
+    4) docker ps -a ;;
+    5) docker stats ;;
+    6) container_id=$($DIALOG --inputbox "Entrez l'ID ou le nom du conteneur :" 8 40 2>&1 >/dev/tty)
+       docker inspect $container_id ;;
+    7) sub_choix=$($DIALOG --clear --backtitle "Sauvegarde et restauration" \
+        --title "Choisissez une option" \
+        --menu "Veuillez choisir une option :" 10 40 2 \
+        1 "Sauvegarder un conteneur" \
+        2 "Restaurer un conteneur" \
+        2>&1 >/dev/tty)
+       clear
+       case $sub_choix in
+           1) container_id=$($DIALOG --inputbox "Entrez l'ID ou le nom du conteneur :" 8 40 2>&1 >/dev/tty)
+              docker commit $container_id ${container_id}_backup
+              docker save -o ${container_id}_backup.tar ${container_id}_backup ;;
+           2) backup_path=$($DIALOG --inputbox "Entrez le chemin du fichier de sauvegarde :" 8 40 2>&1 >/dev/tty)
+              docker load -i $backup_path ;;
+       esac ;;
 esac
-
