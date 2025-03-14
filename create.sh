1dc6549b0ea54a8c7814f08134e5742668ddbb3c
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-14 11:03:26 +0100

    Update create.sh

--- create.sh
+++ create.sh
@@ -7,8 +7,6 @@ choix=$($DIALOG --clear --backtitle "Création/installation de conteneurs" \
     1 "Créer un conteneur avec fichier yaml" \
     2 "Installer un conteneur multi-service (MySQL, phpMyAdmin, FTP, Apache)" \
     3 "Installer un conteneur en session interactive ou détaché" \
-    4 "Supprimer un conteneur" \
-    5 "Supprimer une image Docker" \
     2>&1 >/dev/tty)
 if [ $? -ne 0 ]; then
     dcs
@@ -20,16 +18,4 @@ case $choix in
     1) compose_aio ;;
     2) compose ;;
     3) docker_aio ;;
-    4) container_id=$($DIALOG --inputbox "Entrez l'ID ou le nom du conteneur à supprimer :" 8 40 2>&1 >/dev/tty)
-    if [ $? -ne 0 ]; then
-    create
-    exit
-    fi
-       docker rm -f $container_id ;;
-    5) image_id=$($DIALOG --inputbox "Entrez l'ID ou le nom de l'image Docker à supprimer :" 8 40 2>&1 >/dev/tty)
-    if [ $? -ne 0 ]; then
-        create
-        exit
-    fi
-       docker rmi $image_id ;;
 esac
