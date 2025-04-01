c03f7b8954b2ffb534434a3082a696d3a86fad02
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-04-01 15:30:13 +0200

    Update setdf.sh

--- dockerfile/setdf.sh
+++ dockerfile/setdf.sh
@@ -4,4 +4,4 @@ read -p "nom du conteneur: " name
 read -p "port du conteneur: " portd
 read -p "port a utiliser: " portd
 docker image build -t $name .
-docker container run -it -p $port:$portd $name
+docker container run -d -p $port:$portd $name
