848540af6162b35adc8a68cb96853ea8e67b9dce
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-04-01 18:31:12 +0200

    Update and rename setdf.sh to dfile.sh

--- dockerfile/dfile.sh
+++ dockerfile/dfile.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #If doesn't work execute command "DOCKER_BUILDKIT=0
+read -p "quel dockerfile utiliser ? " folder
 read -p "nom du conteneur: " name
 read -p "port du conteneur: " portd
 read -p "port a utiliser: " port
+cd $folder/
 docker image build -t $name .
 docker container run -d -p $port:$portd $name
